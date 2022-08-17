afd464a5f99176acba1ad46b858a549ec3d2d253
john <jmwangi288@gmail.com>
2022-08-17 18:12:50 +0300

    NOSTORY: supporting resource minor fix

--- tests/supportingResources/main.tf
+++ tests/supportingResources/main.tf
@@ -1,4 +1,4 @@
-module "ecs_vpc" {
+module "docdb_vpc" {
   source               = "boldlink/vpc/aws"
   version              = "2.0.3"
   name                 = local.name
@@ -10,18 +10,3 @@ module "ecs_vpc" {
   availability_zones   = local.azs
   tags                 = local.tags
 }
-
-/*
-module "docdb_vpc" {
-  #count = local.create_vpc ? 1 : 0
-  source               = "git::https://github.com/boldlink/terraform-aws-vpc.git?ref=2.0.3"
-  name                 = "${local.cluster_name}-vpc"
-  account              = data.aws_caller_identity.current.account_id
-  region               = data.aws_region.current.name
-  cidr_block           = local.cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  private_subnets      = local.docdb_subnets
-  availability_zones   = local.azs
-}
-*/

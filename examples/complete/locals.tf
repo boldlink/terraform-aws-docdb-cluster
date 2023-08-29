97595b4eac05e566e9321812e3b42f32da6f5dcb
john <jmwangi288@gmail.com>
2023-08-29 18:48:01 +0300

    showcase sg rules in complete example

--- examples/complete/locals.tf
+++ examples/complete/locals.tf
@@ -3,7 +3,8 @@ locals {
   cluster_name              = "example-complete-cluster"
   supporting_resources_name = "terraform-aws-docdb"
   vpc_id                    = data.aws_vpc.supporting.id
-  subnet_ids                = flatten(data.aws_subnets.private.ids)
-  azs                       = [for subnet in data.aws_subnet.private : subnet.availability_zone]
+  subnet_ids                = flatten(data.aws_subnets.database.ids)
+  azs                       = [for subnet in data.aws_subnet.database : subnet.availability_zone]
+  vpc_cidr = data.aws_vpc.supporting.cidr_block
   tags                      = merge({ "Name" = local.cluster_name }, var.tags)
 }
